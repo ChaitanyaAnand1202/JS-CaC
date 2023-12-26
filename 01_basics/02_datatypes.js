0132fd75ec8b02c2d90be797bf1c471b0f47a47b
CHAITANYA ANAND <anandchaitanya12@gmail.com>
2023-12-26 14:54:10 +0530

    basic datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // only allows the latest version of JS to run
+
+/*
+number => 2 power 53
+string => " " or ''
+boolean => truw/false
+null => standalone value -> represents the absence of a value
+undefined => means no value has been assigned to the variable -> both a value and a data type
+symbol => used to differentiate unique objects (used in React applications)
+object
+*/
+
+let name = "Chaitanya Anand"
+let age = 23
+let goal;
+let score = null;
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof goal) // undefined
+console.log(typeof undefined) // undefined
+console.log(typeof score) // object
+console.log(typeof null) // object
